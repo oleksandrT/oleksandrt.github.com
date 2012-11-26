db7be35003dfdd4b02d620d5eab3d64d246a55da
oleksandrT <tkachenkowork@gmail.com>
2012-11-26 22:25:43 +0200

    new task

---
+++ js/data.js
@@ -0,0 +1,18 @@
+function Datas() {
+
+	datas = [
+		{name: 'Павел Тру', age: '35', salary: '4500', city: 'Киев', phone: '050 334 01 20', email: 'trouegh@mail.ru', sites: 'www.vk.com/id332233', birthday: '13 декабря 1977', about: 'Чувак с выставки высоких технологий в Москве'},
+		{name: 'Виктор Бемпак', age: '25', salary: '3000', city: 'Львов', phone: '068 214 01 30', email: 'viktor.bepmak@gmail.com', sites: 'www.facebook.com/id332233', birthday: '13 мая 1977', about: 'Стремный'},
+		{name: 'Михаил Созновский', age: '33', salary: '2500', city: 'Пырятин', phone: '050 444 61 73', email: 'mih@gmail.com', sites: 'www.vk.com/id332233', birthday: '13 октября 1977', about: 'Глава отдела отделов компании по управлению отделов'},
+		{name: 'Джон Гудмен', age: '29', salary: '5000', city: 'Чикаго', phone: '097 836 94 33, (09045) 3 34 2, 332 23 2', email: 'john_good@yahoo.com', sites: 'www.vk.com/id332233', birthday: '13 ноября 1977', about: 'Чувак с отдела высоких технологий в Минске'},
+		{name: 'Анастасия Польких', age: '19', salary: '3550', city: 'Одесса', phone: '093 574 49 86', email: 'polkih@gmail.com', sites: 'www.vk.com/id332233', birthday: '13 апреля 1977', about: 'Ударница, в переди планеты всей'},
+		{name: 'Константин Бам', age: '42', salary: '2600', city: 'Ялта', phone: '068 558 49 49', email: 'kostantinn.bum-bah@mail.ru', sites: 'www.vk.com/id332233', birthday: '13 января 1977', about: 'Владелец небольшой компании по производству прикладных инструментов для стекло-технической сферы производства. Предпочитает безналичный рассчет.'},
+		{name: 'Уля Васнецова', age: '29', salary: '3750', city: 'Житомир', phone: '097 497 55 55', email: 'uliya@mail.ru', sites: 'www.vk.com/id332233', birthday: '13 августа 1977', about: 'Менеджер компании'},
+		{name: 'Оксана Ганза', age: '31', salary: '4221', city: 'Львов', phone: '044 588 57 67', email: 'oks-87@gmail.com, oks-oks@mail.ru', sites: 'www.vk.com/id332233', birthday: '13 декабря 1977', about: 'Принимала участие в конкурсе'},
+		{name: 'Руслан Пипенко', age: '26', salary: '7000', city: 'Киев', phone: '069 444 44 47', email: 'ru.s.lan@gmail.com', sites: 'www.vk.com/id332233', birthday: '13 июня 1977', about: 'Чувак с выставки высоких технологий в Москве'},
+		{name: 'Алла Этая', age: '20', salary: '3800', city: 'Киев', phone: '050 575 51 86', email: 'allaE@gmail.com', sites: 'www.vk.com/id332233', birthday: '13 октября 1977', about: 'Озарница'}
+	]
+
+	return {datas: datas}
+
+}
